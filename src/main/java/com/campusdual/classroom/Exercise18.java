4aa065a4dbb25cd8936c3141ee6e45d293e6473f
Brayan Iglesias <iglesiasbrayan2@gmail.com>
2024-09-26 18:32:01 +0200

    Ej 18 ok

--- src/main/java/com/campusdual/classroom/Exercise18.java
+++ src/main/java/com/campusdual/classroom/Exercise18.java
@@ -2,7 +2,35 @@ package com.campusdual.classroom;
 
 public class Exercise18 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-	}
+    }
+
+    //Crea y inicializa un array
+    public static int[] createAndInitializeArray(int index) {
+
+        //Se crea el array
+        int[] arrayInt = new int[index];
+        for (int i = 0; i < arrayInt.length; i++) {
+            arrayInt[i] = i + 1;
+        }
+
+        return arrayInt;
+    }
+
+    //Recorre el array y lo imprime por pantalla
+    public static void showInlineArray(int[] array) {
+
+        //StringBuilder para guardar los numeros del array
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < array.length; i++) {
+
+            sb.append(array[i]);
+
+            if (i < array.length - 1) sb.append(" ");
+
+        }
+        System.out.println(sb);
+
+    }
 }
